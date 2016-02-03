846335764bf0a68f7fd0ad73abdfbdd7733ef442
Ben Batschelet <ben@codeup.com>
2016-02-03 09:16:00 -0600

    Specs for rbenv.
    Crap. I'm testing implementation aren't I...

--- spec/bootstrap.rb
+++ spec/bootstrap.rb
@@ -9,16 +9,17 @@ set :host,        options[:host_name]
 set :ssh_options, options
 
 # Disable sudo
-set :disable_sudo, false
+set :disable_sudo, true
 
 # Set environment variables
-# set :env, :LANG => 'C', :LC_MESSAGES => 'C'
+set :env, :RBENV_VERSION => "2.3.0"
 
 # Set PATH
 set :path, '/sbin:/usr/local/sbin:/usr/local/bin:$PATH'
 
 shared_examples "nginx::config" do
   describe command("nginx -t") do
+    let(:disable_sudo) { false }
     # stderr?? Wtf nginx.
     its(:stderr) { should match /configuration file \/etc\/nginx\/nginx\.conf syntax is ok/ }
     its(:stderr) { should match /configuration file \/etc\/nginx\/nginx\.conf test is successful/ }
--- spec/ruby-spec.rb
+++ spec/ruby-spec.rb
@@ -10,3 +10,32 @@ end
 describe "Nginx config should be valid" do
   include_examples "nginx::config"
 end
+
+describe command(". /etc/profile.d/rbenv.sh && ruby -e \"puts 'ruby installed'\"") do
+  its(:stdout) { should eq "ruby installed\n" }
+
+  its(:exit_status) { should eq 0 }
+end
+
+describe command(". /etc/profile.d/rbenv.sh && ruby --version") do
+  its(:stdout) { should match /^ruby 2\.3\.0/ }
+
+  its(:exit_status) { should eq 0 }
+end
+
+describe command(". /etc/profile.d/rbenv.sh && env") do
+  its(:stdout) { should match /^RAILS_ENV=development$/ }
+end
+
+describe command(". /etc/profile.d/rbenv.sh && gem list") do
+  its(:stdout) { should match /^bundler/ }
+  its(:stdout) { should match /^rack/ }
+  its(:stdout) { should match /^rake/ }
+  its(:stdout) { should match /^sass/ }
+end
+
+describe command('printf "GET / HTTP/1.1\nHost: ruby-test.dev\n\n" | nc 127.0.0.1 80') do
+  its(:stdout) { should match /^+HTTP\/1\.1 200 OK$/ }
+
+  its(:stdout) { should match /Phusion Passenger is serving Ruby 2\.3\.0 code on ruby-test\.dev/ }
+end
