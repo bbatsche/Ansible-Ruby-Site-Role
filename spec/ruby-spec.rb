dc15430eefba0b0ab625b33df69aa77ed80c1849
Ben Batschelet <bartleby84@gmail.com>
2018-01-24 21:51:35 -0600

    Update spec code

--- spec/ruby-spec.rb
+++ spec/ruby-spec.rb
@@ -1,14 +1,16 @@
-require_relative "lib/ansible_helper"
 require_relative "bootstrap"
 
 RSpec.configure do |config|
   config.before :suite do
-    AnsibleHelper.instance.playbook "playbooks/ruby-playbook.yml", { copy_configru: true, ruby_version: "2.3.0" }
+    AnsibleHelper.playbook("playbooks/ruby-playbook.yml", ENV["TARGET_HOST"], {
+      copy_configru: true,
+      ruby_version: "2.3.0"
+    })
   end
 end
 
 describe "Nginx config should be valid" do
-  include_examples "nginx::config"
+  include_examples "nginx"
 end
 
 describe command("ruby -e \"puts 'ruby installed'\"") do
